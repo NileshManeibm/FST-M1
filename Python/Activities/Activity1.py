f6e5e6b589404916efa8c6e8fd6a16c1fab81ff0
NileshManeibm <Nilesh.mane@ibm.com>
2024-06-10 13:35:50 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,6 @@
+name = input("What is your Name: ");
+age = int(input("How old you are : "));
+
+year = str((2020-age)+100)
+
+print(name +"will be 100 years old in the year " +year)
---
+++ Python/Activities/Activity2.py
@@ -0,0 +1,8 @@
+num= int(input("Enter the number :"))
+
+
+reminder = num %2 ;
+if(reminder>0):
+  print("you entered even number : " )
+else:
+  print("you entered odd Number :" )
